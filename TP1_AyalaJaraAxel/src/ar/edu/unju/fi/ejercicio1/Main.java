11670c9547142ee4c6c2bfb8bbfab2551b2a03e6
axelayala <axelcapofg@gmail.com>
2024-04-13 16:43:58 -0300

    Commit de ejercicio 1

---
+++ TP1_AyalaJaraAxel/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1 = 8, n2 = 5, n3 = 1, n4 = 9, n5 = 20;
+
+		double promedio = (double) (n1 + n2) / 2;
+		System.out.println(n1 + "," + n2 + "," + n3 + "," + n4 + "," + n5);
+		System.out.println("El promedio es: " + promedio);
+
+	}
+
+}
