e821b88eec47655b2ec3e3f3c0d30fd72aab25c3
axelayala <axelcapofg@gmail.com>
2024-04-20 21:37:59 -0300

    Commit del ejercicio 11

---
+++ TP1_AyalaJaraAxel/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio11.main;
+
+public class Main {
+	public static void main(String[] args) {
+		int j = 40;
+		for (int i = 2; j >= 0; i += 2, j -= 2) {
+			System.out.print((j * i) + " ");
+		}
+		for (int i = 0; j < 0; i += 2, j -= 2) {
+			System.out.print((j * i) + " ");
+		}
+	}
+}
